d08383dd3f049893ccc41f62819e0e16de0cfa72
Álvaro Membrillo Pérez <alvaro.membrillo@gmail.com>
2026-05-10 01:00:34 +0200

    Archivos básicos del proyecto y cambios en gitignore

---
+++ src/main/java/com/tareapp/MainApp.java
@@ -0,0 +1,27 @@
+package com.tareapp;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		Label label = new Label("Tablero Kanban");
+		
+		StackPane root = new StackPane(label);
+		Scene scene = new Scene(root, 900, 600);
+		
+		stage.setTitle("Tareapp");
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch();
+	}
+    
+}
